0286ec87d449eb0872ffd6ea300443eeb46f5f38
connieuribe <conniuribe95@gmail.com>
2023-04-19 05:50:19 +0000

    challenge 02

---
+++ ops-challenge02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import requests
+from requests.exceptions import HTTPError
+
+# Script: Ops 301 Class 12 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 28 Mar 2023
+# Purpose: HTTP Request
+# Reference: https://realpython.com/lessons/responses-status-codes/
+# -------------------------------

4f236ce074b969056ed4c8d91bf46de9159cc106
connieuribe <conniuribe95@gmail.com>
2023-04-19 07:27:52 +0000

    code 02

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Code challenges for Code Fellows Ops 401
 
 |    Content   | Language      | File          |
 |   ---------  |   -------     |   -------     |
-| Print Hello World | Python | []() |
+| Uptime Sensor Tool Part 1 of 2 | Python | [Challenge 02](https://github.com/connieuribe/ops-401d6-code-challenges/blob/main/ops-challenge02.py) |
+|       |       |           |
--- ops-challenge02.py
+++ ops-challenge02.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python3
-import requests
-from requests.exceptions import HTTPError
-
-# Script: Ops 301 Class 12 Ops Challenge Solution
+import os, time
+from datetime import datetime
+# Script: Ops 401 Class 02 Ops Challenge Solution
 # Author: Connie Uribe Chavez
-# Date of lates revision: 28 Mar 2023
-# Purpose: HTTP Request
-# Reference: https://realpython.com/lessons/responses-status-codes/
+# Date of lates revision: 18 Apr 2023
+# Purpose: an uptime sensor tool that checks systems are responding. 
+# Reference: 
 # -------------------------------
+
+#main
+
+
+
+def check_ping(a):
+    # Transmit a single ICMP (ping) packet to a specific IP every two seconds.
+    respond = os.system("ping -c 1 " + a)
+    time.sleep(2)
+    # Evaluate the response as either success or failure.
+    # Assign success or failure to a status variable.
+    if respond == 0:
+        ping_status = "network is active"
+    else:
+        ping_status = "network is down"
+    return ping_status
+
+# For every ICMP transmission attempted, print the status variable along with a comprehensive timestamp and destination IP tested.
+# Example output: 2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
+while True:
+    ip_a = input("Enter an IP address: ")
+    status = check_ping(ip_a)
+    currenttime = datetime.now()
+    print(currenttime, status, " to ", ip_a)
+
+
+
+
+
+
+#end

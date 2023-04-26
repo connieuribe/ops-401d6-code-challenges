73ab40ca28833e1bbc7bcbf3a323f21ab2978666
connieuribe <connieuribe95@gmail.com>
2023-04-26 05:27:04 +0000

    new file

---
+++ ops-challenge07.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from cryptography.fernet import Fernet
+# Script: Ops 401 Class 06 Ops Challenge Solution
+# Author: Marco Vazquez, edited by Connie Uribe Chavez
+# Date of lates revision: 24 Apr 2023
+# Purpose: File Encryption Script Part 1 of 3
+# Reference: 
+# -------------------------------

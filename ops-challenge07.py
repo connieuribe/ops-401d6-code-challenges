ea7a633ef5bda7945388869cff5edf5912b41031
connieuribe <connieuribe95@gmail.com>
2023-04-27 03:14:13 +0000

    updates to code

--- ops-challenge07.py
+++ ops-challenge07.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 from cryptography.fernet import Fernet
-# Script: Ops 401 Class 06 Ops Challenge Solution
-# Author: Marco Vazquez, edited by Connie Uribe Chavez
-# Date of lates revision: 24 Apr 2023
+import os
+# Script: Ops 401 Class 07 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 26 Apr 2023
 # Purpose: File Encryption Script Part 2 of 3
 # Reference: 
 # ------------------------------- 
 
 # Add a feature capability to your script to:
-
+# for each file in the files list, encrypt the data.
 # Recursively encrypt a single folder and all its contents.
+startDir = "/temp"
+for root, dirs, files in os.walk(startDir):
+    for fileName in files:
+        print("File name: " + fileName)
+        # Encrypt files
+    for dirName in dirs:
+        print("Directory name: " + dirName)
+
+
 # Recursively decrypt a single folder that was encrypted by this tool.

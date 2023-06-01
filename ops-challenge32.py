bdfe310aa5716267910a89854a4c4db6cff0008d
Connie Uribe Chavez <47328484+connieuribe@users.noreply.github.com>
2023-05-31 19:08:05 -0500

    Create ops-challenge32.py

---
+++ ops-challenge32.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Script: Ops 401 Class 33 Ops Challenge Solution
+# Author: Used Demo code, edited by Connie Uribe Chavez
+# Date of latest revision: 30 May 2023
+# Purpose: 
+# Purpose: 

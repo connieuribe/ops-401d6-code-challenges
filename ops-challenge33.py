d03276082c41715a232585504dd9a93e506614df
connieuribe <connieuribe95@gmail.com>
2023-06-01 21:16:28 +0000

    comment

--- ops-challenge33.py
+++ ops-challenge33.py
@@ -4,3 +4,9 @@
 # Date of latest revision: 01 Jun 2023
 # Purpose: Signature-based Malware Detection Part 3 of 3
 # Purpose: development of my own basic antivirus tool in Python.
+############################################################
+# Successfully connect to the VirusTotal API
+# Automatically compare your target file’s md5 hash with the hash values of entries on VirusTotal API
+# Print to the screen the number of positives detected and total files scanned
+# The script must successfully execute on both Ubuntu Linux 20.04 Focal Fossa and Windows 10.
+############################################################

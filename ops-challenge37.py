45b787f63b5b7bd0ea14f030b27ea7e15d170d21
Connie Uribe Chavez <47328484+connieuribe@users.noreply.github.com>
2023-06-07 23:21:24 -0500

    Update ops-challenge37.py

--- ops-challenge37.py
+++ ops-challenge37.py
@@ -1,1 +1,53 @@
 
+#!/usr/bin/env python3
+
+# The below Python script shows one possible method to return the cookie from a site that supports cookies.
+
+import requests
+import webbrowser
+# targetsite = input("Enter target site:") # Uncomment this to accept user input target site
+targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
+response = requests.get(targetsite)
+cookie = response.cookies
+
+
+
+def cookiesFunc():
+      # Send the cookie back to the site and receive a HTTP response
+    response = requests.get(targetsite, cookies=cookie)
+
+    # Generate a .html file to capture the contents of the HTTP response
+    filename = "response.html"
+    with open(filename, "w") as file:
+        file.write(response.text)
+
+    # Open the HTML file with Firefox
+    firefox_path = "path_to_firefox_executable"  # Replace with the actual path to your Firefox executable
+    webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path), 1)
+    webbrowser.get('firefox').open(filename)
+
+def bringforthcookiemonster(): # Because why not!
+    print('''
+
+              .---. .---.
+             :     : o   :    me want cookie!
+         _..-:   o :     :-.._    /
+     .-''  '  `---' `---' "   ``-.
+   .'   "   '  "  .    "  . '  "  `.
+  :   '.---.,,.,...,.,.,.,..---.  ' ;
+  `. " `.                     .' " .'
+   `.  '`.                   .' ' .'
+    `.    `-._           _.-' "  .'  .----.
+      `. "    '"--...--"'  . ' .'  .'  o   `.
+
+        ''')
+
+bringforthcookiemonster()
+print("Target site is " + targetsite)
+print(cookie)
+cookiesFunc()
+
+# Add here some code to make this script perform the following:
+# - Send the cookie back to the site and receive a HTTP response
+# - Generate a .html file to capture the contents of the HTTP response
+# - Open it with Firefox
